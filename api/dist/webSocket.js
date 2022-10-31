ea0506b41fc72d99feac1d4d094a1bbc292a37d0
Ivan <ivanpronin1379@gmail.com>
2022-10-31 16:43:51 +0500

    commit before reseting dir

--- api/dist/webSocket.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const ws_1 = __importDefault(require("ws"));
-const index_1 = require("./index");
-const wss = new ws_1.default.Server({ server: index_1.server });
-//# sourceMappingURL=webSocket.js.map
